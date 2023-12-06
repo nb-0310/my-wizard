83a51998b78b049735404d7a86032f2456b597c2
Nirzar Bhatt <nirzarbhatt031093@gmail.com>
2023-12-06 18:00:25 +0530

    En: Sign

--- src/app/services/sign.service.ts
+++ src/app/services/sign.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ethers } from 'ethers';
-import { environment } from '../../environments/environment';
+// import { environment } from '../../environments/environment';
 
 interface CustomWindow extends Window {
   ethereum?: any;
@@ -16,28 +16,28 @@ export class SignService {
 
   async connectToWeb3(): Promise<void> {
     try {
-      // const customWindow = window as CustomWindow;
+      const customWindow = window as CustomWindow;
 
-      const provider = new ethers.providers.JsonRpcProvider(environment.RpcUrl);
-      console.log('Provder rpc', provider);
-      this.signer = new ethers.Wallet(environment.privateKey, provider);
-      console.log('Signer Rpc', this.signer);
+      // const provider = new ethers.providers.JsonRpcProvider(environment.RpcUrl);
+      // console.log('Provder rpc', provider);
+      // this.signer = new ethers.Wallet(environment.privateKey, provider);
+      // console.log('Signer Rpc', this.signer);
 
       // This will be used in production
 
-      // if (customWindow.ethereum) {
-      //   const accounts = await customWindow.ethereum.request({
-      //     method: 'eth_requestAccounts',
-      //   });
+      if (customWindow.ethereum) {
+        const accounts = await customWindow.ethereum.request({
+          method: 'eth_requestAccounts',
+        });
 
-      //   const provider = new ethers.providers.Web3Provider(
-      //     customWindow.ethereum
-      //   );
-      //   await provider.send('eth_requestAccounts', []);
-      //   this.signer = provider.getSigner();
-      // } else {
-      //   console.error('Ethereum provider not detected');
-      // }
+        const provider = new ethers.providers.Web3Provider(
+          customWindow.ethereum
+        );
+        await provider.send('eth_requestAccounts', []);
+        this.signer = provider.getSigner();
+      } else {
+        console.error('Ethereum provider not detected');
+      }
     } catch (error) {
       console.error('Error connecting to Ethereum provider:', error);
     }
